cd472f390954b69f420f4f3c885988b51242cea0
Simon <SimonHel@outlook.de>
2025-03-20 10:32:08 +0100

    init

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,50 @@
 import Fastify from 'fastify';
-import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
-import { join } from 'path';
+import fs from 'fs'
+import path from 'path';
 
 const fastify = Fastify({
   logger: true
+})
+
+fastify.get('/', (_, reply) => {
+  const filePath = path.join('src', 'test.pdf');
+
+  if (!fs.existsSync(filePath)) {
+    return reply.status(404).send({ error: 'File not found' });
+  }
+
+  const readStream = fs.createReadStream(filePath);
+  const stats = fs.statSync(filePath);
+
+  readStream.on('error', (err) => {
+    console.error('Error reading file:', err);
+    reply.status(500).send({ error: 'Internal server error while reading file' });
+  });
+
+  readStream.on('open', () => {
+    console.log("---------------------------------------------")
+    console.log('File stream opened successfully');
+  });
+
+  readStream.on('data', (chunk) => {
+    console.log("---------------------------------------------")
+    console.log('Data chunk received:', chunk.length);
+  });
+
+  readStream.on('end', () => {
+    console.log("---------------------------------------------")
+    console.log('Stream finished');
+  });
+
+  reply
+    .header('Content-Type', 'application/octet-stream')
+    .header('Content-Disposition', 'attachment; filename="test.pdf"')
+    .header('Content-Length', stats.size)
+    .send(readStream);
 });
 
-const pluginOptions: Partial<AutoloadPluginOptions> = {
-  // Place your custom options the autoload plugin below here.
-}
-
-fastify.register(AutoLoad, {
-  dir: join(__dirname, 'plugins'),
-  options: pluginOptions
-});
-
-fastify.register(AutoLoad, {
-  dir: join(__dirname, 'routes'),
-  options: pluginOptions
-});
-
-fastify.listen({ host: '::', port: Number(process.env.PORT) || 3000 }, function (err, address) {
+fastify.listen({ host: '::', port: Number(process.env.PORT) || 3001 }, function (err, address) {
+  console.log("---------------------------------------------")
   if (err) {
     fastify.log.error(err)
     process.exit(1)
